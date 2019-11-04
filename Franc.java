5923e7bd79da109e5ade218169de56b419f6b901
Marcus V. Oliveira <marcusvoliveira@dcc.ufmg.br>
2019-11-04 10:55:27 -0300

    Commit (5)

--- Franc.java
+++ Franc.java
@@ -1,10 +1,16 @@
 public class Franc extends Money {
 
+	private String currency;
+
 	Franc(int amount, String currency) {
 		super(amount, currency);
 	}
 
 	Money times(int multiplier) {
 		return Money.franc(amount * multiplier);
+	}
+
+	String currency() {
+		return currency;
 	}
 }
--- Money.java
+++ Money.java
@@ -1,25 +1,26 @@
-abstract class Money { 
-    protected int amount;
-    protected String currency;
+abstract class Money {
+	protected int amount;
+	private String currency;
 
 	public boolean equals(Money object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
-    }
+	}
+
 	static Money dollar(int amount) {
 		return new Dollar(amount, "USD");
 	}
 
 	static Money franc(int amount) {
 		return new Franc(amount, "CHF");
-    }
-    
-    abstract Money times(int multiplier);
+	}
 
 	Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
+
+	abstract Money times(int multiplier);
 
 	String currency() {
 		return currency;
